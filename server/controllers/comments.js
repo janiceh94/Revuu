cfc60e585e29ffd6801fd337b0844bd732de814c
evankeith <evanmkeith@gmail.com>
2022-03-14 21:14:11 -0700

    Created comments CRUD

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -1,28 +1,73 @@
 const db = require('../models');
 
-const showOne = () => {
-    db.User
-}
+// const showAll = (req, res) => {
 
-const showAll = () => {
+// }
 
-}
+// const showOne = () => {
+    
+// }
 
-const create = () => {
-
+const create = (req, res) => {
+    db.Review.findById(req.params.rid, (err, foundReview) => {
+        if(err) {
+            return res.status(400).json({
+                message: "Nope",
+                error: err
+            })
+        }
+        const newComment = req.body;
+        foundReview.comments.push(newComment);
+        return res.status(201).json({
+            message: "Successfully created!",
+            data: newComment
+        })
+    })
 }
 
 const update = () => {
+    db.Review.findById(req.params.rid, (err, foundReview) => {
+        if(err) {
+            return res.status(400).json({
+                message: "Nope",
+                error: err
+            })
+        }
+        const updatedComment = req.body;
+        const commentIdx = foundReview.comments.findIndex((comment) => comment._id === req.params.cid);
 
+        foundReview.comments.splice(commentIdx, 1, updatedComment);
+
+        return res.status(202).json({
+            message: "Successfully updated!",
+            data: updatedComment
+        })
+    })
 }
 
 const destroy = () => {
+    db.Review.findById(req.params.rid, (err, foundReview) => {
+        if(err) {
+            return res.status(400).json({
+                message: "Nope",
+                error: err
+            })
+        }
+        const commentIdx = foundReview.comments.findIndex((comment) => comment._id === req.params.cid);
+        const deletedComment = foundReview.comments[commentIdx];
 
+        foundReview.comments.splice(commentIdx, 1);
+
+        return res.status(201).json({
+            message: "Successfully deleted!",
+            data: deletedComment
+        })
+    })
 }
 
 module.exports = {
-    showOne, 
-    showAll, 
+    // showAll,
+    // showOne,  
     create, 
     update, 
     destroy
--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const {comments} = require('../controllers');
 
-router.get('/', comments.showAll);
-router.get('/:id', comments.showOne);
-router.post('/', comments.create);
-router.put('/:id', comments.update);
-router.delete('/:id', comments.destroy);
+// router.get('/', comments.showAll);
+// router.get('/:id', comments.showOne);
+router.post('/:rid', comments.create);
+router.put('/:rid/:cid', comments.update);
+router.delete('/:rid/:cid', comments.destroy);
 
 module.exports = router;
