68a2f0812eaa1736143ac21503ea7b45a663d589
Sammark1 <samaustinmark@gmail.com>
2022-03-15 21:38:48 -0400

    added reference to commented reviews to user data

--- server/models/Review.js
+++ server/models/Review.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new Schema ({
     body: {type: String, required: true}, 
-    user: {type: Schema.Types.ObjectId, ref: "User"}, 
+    user: {type: Schema.Types.ObjectId, ref: "User", required: true}, 
+    //REVIEW CREATE ROUTES AND CONTROLLERS FOR LIKES???
     likes: [{type: Schema.Types.ObjectId, ref: "User"}],
 })
 
