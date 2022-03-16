d47a4d6119d4eaefb5b9654251b69fd31491cd4f
Sammark1 <samaustinmark@gmail.com>
2022-03-16 19:43:10 -0400

    added copyright component

--- client/src/components/FakePage/index.js
+++ client/src/components/FakePage/index.js
@@ -1,9 +1,11 @@
+import FooterCR from "../FooterCR";
 import FooterSticky from "../FooterSticky";
 import "./FakePage.css";
 function App() {
   return (
     <div className="App">
       <FooterSticky/>
+      <FooterCR/>
     </div>
   );
 }
