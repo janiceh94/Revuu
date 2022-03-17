f15054c13ecdba8abbef3147e90cc3682b41ec01
janiceh94 <haesung94@gmail.com>
2022-03-16 19:27:59 -0700

    Created pages

--- client/src/components/FakePage/index.js
+++ client/src/components/FakePage/index.js
@@ -1,12 +1,14 @@
 import FooterCR from "../FooterCR";
 import FooterSticky from "../FooterSticky";
 import Header from "../Header";
+import Home from '../../page/Home';
 import "./FakePage.css";
 function App() {
   return (
     <div className="App">
       <Header/>
       <FooterSticky/>
+      <Home/>
       <FooterCR/>
     </div>
   );
