fe0c0f425d9d81be0eb845d988c2d4d3c9169060
Sammark1 <samaustinmark@gmail.com>
2022-03-16 21:03:46 -0400

    added initial global CSS

--- client/src/components/FakePage/index.js
+++ client/src/components/FakePage/index.js
@@ -4,6 +4,9 @@ import "./FakePage.css";
 function App() {
   return (
     <div className="App">
+      <h1>Fake Page for Testing whatever you want. Make sure to delete me.</h1>
+      <p>ptag</p>
+      
       <FooterSticky/>
       <FooterCR/>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import FakePage from './components/FakePage';
+import "./style.css";
 
 ReactDOM.render(
   <BrowserRouter>
