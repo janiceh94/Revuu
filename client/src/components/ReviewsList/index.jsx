d62f33064369e0457473992fe8c073d94803a29c
Sammark1 <samaustinmark@gmail.com>
2022-03-17 22:39:22 -0400

    successful stream of data from DB to front-end

--- client/src/components/ReviewsList/index.jsx
+++ client/src/components/ReviewsList/index.jsx
@@ -4,17 +4,28 @@ import { useEffect, useState } from "react";
  
 function ReviewsList(){
 	
+	const [reviews, setReviews] = useState([]);
 	const [title, setTitle] = useState("");
 
 	const getReviews = async (e) => {
 		e.preventDefault();
 		await apiClient.get('/api/review').then((res)=>{
-			console.log (res);
+			console.log (res.data.data);
+			setReviews(res.data.data);
 		})
 	}
 
+	// useEffect(() => {
+	// 	getReviews();
+	// }, []);
+
 	return(
-		<button onClick={getReviews}>Get Reviews</button>
+		<div>
+			<button onClick={getReviews}>Get Reviews</button>
+			{reviews.map((review, i)=>{
+				return<h3 key={i}>{review.title}</h3>;
+			})}
+		</div>
 	);
 }
 export default ReviewsList;
