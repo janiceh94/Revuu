3565d9adfb4f0ec6695def508d52f74718ae8de9
abled007 <audreybledsoe3@gmail.com>
2022-03-19 10:57:49 -0700

    Fixed bugs

---
+++ client/src/components/Login/index.js
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import * as authService from "../../api/auth.service"
+
+export default function Login({checkUserActive}) {
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        await authService.login(email, password).then(() => {
+            {checkUserActive()}
+            setEmail = ""
+            setPassword = ""
+        })
+    }
+
+    return (
+        <div>
+            <form>
+                <label>Email
+                <input	
+						onChange={(e) => setEmail(e.target.value)}
+						value={email}
+						type="text"
+						name="email"
+						placeholder="email"
+                        />
+                </label>
+                <label>Password
+                 <input	
+						onChange={(e) => setPassword(e.target.value)}
+						value={password}
+						type="text"
+						name="password"
+						placeholder="password"
+                        />
+                </label>
+                <input type="submit" onSubmit={handleSubmit} />
+            </form>
+        </div>
+    )
+}
