2180ebfadf1915ab73cb96d55bbe6a7bad14b428
janiceh94 <haesung94@gmail.com>
2022-03-20 11:57:06 -0700

    Reviews are posted in reverse chronological order

--- client/src/page/Home/index.js
+++ client/src/page/Home/index.js
@@ -1,13 +1,18 @@
 import apiClient from "../../api/axios.config";
 import { useEffect, useState } from "react";
+import * as reviewService from '../../api/review.service';
+import {useNavigate} from 'react-router-dom';
+
 
 export default function Home() {
 	const [reviews, setReviews] = useState([]);
 
+	const navigate = useNavigate();
+
 	const fetchReviews = async () => {
 		await apiClient.get(`/api/review`)
 		.then((res) => {
-			setReviews(res.data.data);
+			setReviews(res.data.data.reverse());
 		});
 	};
 
@@ -18,9 +23,14 @@ export default function Home() {
 	return (
 		<div>
 			<h1>Home</h1>
+			<h2>Header</h2>
 			{reviews.map((review, i) => {
-				return <h3 key={i}>{review.reviewItem}</h3>;
+				return <h3 key={i}>{review.reviewItem}
+				<button onClick = {review._id}>View Review</button>
+				</h3>;
 			})}
+			<h2>FooterSticky</h2>
+			<h2>FooterCR</h2>
 		</div>
 	);
 }
