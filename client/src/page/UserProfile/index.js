72120cd775f33f50177f6785343f22d1f3d4dc29
abled007 <audreybledsoe3@gmail.com>
2022-03-21 15:33:28 -0700

    restored api user sevice and user profile

--- client/src/page/UserProfile/index.js
+++ client/src/page/UserProfile/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import apiClient from "../../api/axios.config";
+import * as userService from '../../api/user.service'
 
-export default function UserProfile(){
+
+const UserProfile = () => {
+    const [user, setUserProfile] = useState({});
+
+    // async function fetchUserProfile() {
+    //     await userService.get().then((res) => {
+    //         setUserProfile(res.data.data);
+    //     })
+    // }
+
+    const fetchUserProfile = async () => {
+		await apiClient.get(`/api/user`)
+		.then((res) => {
+			setUserProfile(res.data.data);
+		});
+	};
+
+    useEffect(() => {
+        fetchUserProfile();
+    }, []);
+
+
     return(
+
         <div>
-            <h2>User Profile</h2>
+           <h1>User Profile</h1>
+
+           <div>Name: {user.name}</div>
+
+           <img className="profile" src="https://jw-webmagazine.com/wp-content/uploads/2020/03/Kimetsu-no-YaibaDemon-Slayer.jpg" />
+           
+            <div>username: {user.username}</div>
+
+           <button>Edit Profile</button><button>Edit review</button>
+
+           <div className="container">reviews
+               <img src="https://storcpdkenticomedia.blob.core.windows.net/media/recipemanagementsystem/media/recipe-media-files/recipes/retail/x17/16714-birthday-cake-600x600.jpg?ext=.jpg"></img>
+               <p>title</p>
+               <p>review/comment</p>
+           </div>
+
         </div>
     )
 }
+
+export default UserProfile;
