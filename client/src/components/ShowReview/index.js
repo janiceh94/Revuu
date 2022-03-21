7b9cd34aa1862213d0523270b5a68805f88bebef
Sammark1 <samaustinmark@gmail.com>
2022-03-21 14:47:02 -0400

    added routing from createReview to showReview

--- client/src/components/ShowReview/index.js
+++ client/src/components/ShowReview/index.js
@@ -11,24 +11,25 @@ export default function MakeReview() {
         await reviewService.get(reviewId)
                 .then((err, foundReview) => {
                     setReview(foundReview);
+                    console.log("foundReview",foundReview);
                 })
     };
 
-    const checkImage = () => {
-        if(review.link !== "https://picsum.photos/500?grayscale"){
-            return <a href={review.link} target="_blank" rel="noreferrer">{review.reviewItem}</a>
-        } else {
-            return <img src={review.link} alt="review-item"/>
-        }
-    }
+    // const checkImage = () => {
+    //     if(review.link !== "https://picsum.photos/500?grayscale"){
+    //         return <a href={review.link} target="_blank" rel="noreferrer">{review.reviewItem}</a>
+    //     } else {
+    //         return <img src={review.link} alt="review-item"/>
+    //     }
+    // }
 
-    const reviewItem = () => {
-        if(review.link !== "https://picsum.photos/500?grayscale"){
-            return 
-        } else {
-            return <h3>Review Item:<br/><>{review.reviewItem}</></h3>
-        }
-    }
+    // const reviewItem = () => {
+    //     if(review.link !== "https://picsum.photos/500?grayscale"){
+    //         return 
+    //     } else {
+    //         return <h3>Review Item:<br/><>{review.reviewItem}</></h3>
+    //     }
+    // }
 
     useEffect(() => {
 		getReviewData();
@@ -37,14 +38,14 @@ export default function MakeReview() {
     return(
         <div> 
             <h1>Show Review</h1>
-            <div id="image">
+            {/* <div id="image">
                 {checkImage()}
             </div>
             <h2>{review.title}</h2>
             {reviewItem()}
             <h4>Category:<br/>{review.category}</h4>
             <p>Description:<br/>{review.body}</p>
-            <h4>Rating:<br/>{review.rating}</h4>
+            <h4>Rating:<br/>{review.rating}</h4> */}
         </div>
     )
 }
