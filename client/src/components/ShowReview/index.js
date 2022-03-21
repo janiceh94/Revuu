2dce4b010549e2d562c571b7ca28a29e0f267b90
evankeith <evanmkeith@gmail.com>
2022-03-20 18:54:30 -0700

    added image title and review item to show

--- client/src/components/ShowReview/index.js
+++ client/src/components/ShowReview/index.js
@@ -6,7 +6,8 @@ import * as reviewService from "../../api/review.service";
 export default function MakeReview({checkUserActive}) {
     const navigate = useNavigate();
     const [ review, setReview ] = useState({
-        link: "https://picsum.photos/200?grayscale",
+        imageLink: "https://picsum.photos/200?grayscale",
+        webLink: "",
         reviewItem: "Shoes",
         title: "I hate these Shoes",
         category: "Clothing",
@@ -22,14 +23,37 @@ export default function MakeReview({checkUserActive}) {
                 })
     };
 
-	useEffect(() => {
+    useEffect(() => {
 		getReviewData();
 	}, []);
+
+    const checkImage = () => {
+        if(review.webLink === ""){
+            return <img src={review.imageLink} />
+        } else {
+            return <a href={review.webLink} target="_blank" rel="noreferrer">{review.reviewItem}</a>
+        }
+    }
+
+    const reviewItem = () => {
+        if(review.webLink === ""){
+            return <>{review.reviewItem}</>
+        } else {
+            return 
+        } 
+    }
 
     return(
         <div> 
             <h1>Show Review</h1>
+            <div id="image">
+                {checkImage()}
+            </div>
+            <h2>{review.title}</h2>
+            <h3>{reviewItem()}</h3>
             
+
+
         </div>
     )
 }
