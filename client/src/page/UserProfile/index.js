6a6e5a0af26c4c70a6b1f13611b13160121a99f7
janiceh94 <haesung94@gmail.com>
2022-03-22 14:13:08 -0700

    some more cleaning

--- client/src/page/UserProfile/index.js
+++ client/src/page/UserProfile/index.js
@@ -17,9 +17,7 @@ export default function UserProfile(){
     const fetchUser = async () => {
 		await authService.getProfile()
 		.then((res) => {
-            console.log("res",res);
 			setUsr(res.data.data);
-            console.log("usr1",usr);
 		});
 	};
 
@@ -59,8 +57,6 @@ export default function UserProfile(){
 	}, []);
 
     useEffect(() => {
-        console.log("usr2", usr);
-        console.log("reviews:", reviews);
 	}, [usr, reviews]);
 
     return(
@@ -80,27 +76,3 @@ export default function UserProfile(){
         </div>
     )
 }
-
-
-{/* <div>
-            <h3>{props.data.title}</h3>
-            <h5>{props.data.reviewItem}</h5>
-            <h5>{props.data.category}</h5>
-            
-            <p>{props.data.body}</p>
-            <div>
-                star rating goes here!
-            </div>
-            <p>{props.data.likes}</p>
-        </div> */}
-
-// firstName: {type: String},
-// //userIcon: {type: String},
-// // userName: {type: String, unique: true},
-// email: {type: String, required: true, unique: true},
-// password: {type: String, required: true, select: false},
-// bio: {type: String},
-// reviews:[{type: Schema.Types.ObjectId, ref:'Review'}],
-// follows: [{type: Schema.Types.ObjectId,ref: "User"}],
-// //NOTE THE COMMENTS KEY REFERS TO REVIEWS YOU HAVE COMMENTED ON, NOT THE COMMENTS THEMSELVES. (CAN'T DO THE COMMENTS UNLESS WE MAKE A NEW REFERENCED SCHEMA)
-// comments:[{type: Schema.Types.ObjectId,ref: "Review"}],
