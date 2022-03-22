f110382be5de5d092865a50778f19b77e69285b3
Sammark1 <samaustinmark@gmail.com>
2022-03-22 01:39:20 -0400

    basic setup that should work but doesn't

--- client/src/page/UserProfile/index.js
+++ client/src/page/UserProfile/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useEffect, useState } from "react";
+import * as authService from "../../api/auth.service"
 
 export default function UserProfile(){
+
+    const [usr, setUsr] = useState({});
+
+    const fetchUser = async () => {
+		await authService.getProfile()
+		.then((res) => {
+            console.log(res);
+			setUsr(res.data.data);
+            console.log("usr",usr);
+		});
+	};
+
+    useEffect(() => {
+		fetchUser();
+	}, []);
+
     return(
         <div>
-            <h2>User Profile</h2>
+            <h2>User profile</h2>
         </div>
     )
 }
